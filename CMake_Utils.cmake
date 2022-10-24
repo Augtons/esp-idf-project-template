fcca456310f26d42a29556d3a8a96cc3f95b2098
Augtons <79037928+Augtons@users.noreply.github.com>
2022-10-24 22:01:58 +0800

    First Commit

---
+++ CMake_Utils.cmake
@@ -0,0 +1,20 @@
+function(first_not_null OUTPUT)
+    foreach(VAL ${ARGN})
+        if (VAL)
+            set(${OUTPUT} ${VAL} PARENT_SCOPE)
+            return()
+        endif()
+    endforeach()
+endfunction()
+
+function(add_path)
+    # Add your configuration for cmake
+    if (WIN32)
+        set(path_separator ";")
+    elseif(UNIX)
+        set(path_separator ":")
+    endif()
+
+    list(JOIN ARGN "${path_separator}" ADDED_PATH)
+    set(ENV{PATH} "${ADDED_PATH}${path_separator}$ENV{PATH}")
+endfunction()
